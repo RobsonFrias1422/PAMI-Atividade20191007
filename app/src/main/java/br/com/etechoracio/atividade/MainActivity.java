9ce0a2adbc667927e423058c2cfeff834ddafc3e
Your Name <you@example.com>
2019-10-09 21:53:27 -0300

    exercicio 1

--- app/src/main/java/br/com/etechoracio/atividade/MainActivity.java
+++ app/src/main/java/br/com/etechoracio/atividade/MainActivity.java
@@ -71,6 +71,7 @@ public class MainActivity extends AppCompatActivity implements CustomDialog.Item
     @Override
     public boolean onMenuItemClick(MenuItem menuItem) {
         adapter.removeItem(selectedItem);
+        adapter.removeItem(selectedItemName);
         return false;
     }
 }
